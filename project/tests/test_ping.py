5e7cd5d0205ddecba7bd437736eaacb44b55533a
David Lopez <dflindc@yahoo.com>
2022-05-28 16:01:07 -0400

    pytest setup, app api router setup

---
+++ project/tests/test_ping.py
@@ -0,0 +1,12 @@
+from app import main
+
+def test_ping(test_app):
+    # given
+    # test_app
+
+    # when
+    response = test_app.get("/ping")
+
+    # then
+    assert response.status_code == 200
+    assert response.json() == {"environment": "dev", "ping": "pong!", "testing": True}
