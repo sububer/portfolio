a02076e827ccd18f3695e08d39a82ce2c686dd26
David Lopez <dflindc@yahoo.com>
2022-05-29 11:02:22 -0400

    add summaries route.. first iteration

--- project/app/main.py
+++ project/app/main.py
@@ -1,9 +1,8 @@
 import logging
 
 from fastapi import FastAPI
-from tortoise.contrib.fastapi import register_tortoise
 
-from app.api import ping
+from app.api import ping, summaries
 from app.db import init_db
 
 log = logging.getLogger("uvicorn")
@@ -12,6 +11,7 @@ log = logging.getLogger("uvicorn")
 def create_application() -> FastAPI:
     application = FastAPI()
     application.include_router(ping.router)
+    application.include_router(summaries.router, prefix="/summaries", tags=["summaries"])
 
     return application
 
--- project/app/models/pydantic.py
+++ project/app/models/pydantic.py
@@ -2,3 +2,6 @@ from pydantic import BaseModel
 
 class SummaryPayloadSchema(BaseModel):
     url: str
+
+class SummaryResponseSchema(SummaryPayloadSchema):
+    id: int
